5f6e74aac8ffc8d51355a2a2d0886e3dbbdb0383
Laura Ortiz <bjtamyl@hotmail.com>
2022-06-26 18:59:32 -0300

    actualizacion1

--- src/components/Empleado.jsx
+++ src/components/Empleado.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import tarjetasEstilos from "../css/tarjetasEstilos.css";
 
 
 function empleado({fullName,picSrc, title, department}) {
     
   return (
-    <div className='card text-center bg-dark'>
-       <img src={picSrc} alt="avatares" />
+    <div className='card text-center bg-dark mt-5'>
+       <img src={picSrc} alt="avatares" className='imagen mt-5' />
         <div className='card-body text-light'>
             <h4 className='card-title'>{fullName}</h4>
             <p>{title}</p>
